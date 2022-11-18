6f9e71bb3e7f8994f8710703e42ac75228c65701
VictorGugg <victor.guggenbuhl@gmail.com>
2022-11-18 15:03:44 +0100

    Quest 12 : Added randomly generated data for fixtures with Faker.

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -7,41 +7,69 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
+use Faker\Factory;
+
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
+    const PROGRAMS = [
+        'The Walking Dead' => 'Action',
+        'Breaking Bad' => 'Drama',
+        'Better Call Saul' => 'Drama',
+        'Love, Death and Robots' => 'Animation',
+        'Sense8' => 'Sci-Fi',
+    ];
+
     public function load(ObjectManager $manager)
     {
-        $program = new Program();
-        $program->setTitle('The Walking Dead');
-        $program->setSynopsis('Zombie apocalypse, humans have to survive, together as well as against each other.');
-        $program->setCategory($this->getReference('category_Action'));
-        $manager->persist($program);
+        $faker = Factory::create();
 
-        $program = new Program();
-        $program->setTitle('Breaking Bad');
-        $program->setSynopsis('High school teacher turns to drug manufacturing following his cancer diagnosis.');
-        $program->setCategory($this->getReference('category_Drama'));
-        $manager->persist($program);
-
-        $program = new Program();
-        $program->setTitle('Better Call Saul');
-        $program->setSynopsis('Spin-off focusing on the lawyer character of Breaking Bad.');
-        $program->setCategory($this->getReference('category_Drama'));
-        $manager->persist($program);
-
-        $program = new Program();
-        $program->setTitle('Love, Death and Robots');
-        $program->setSynopsis('Stand-alone episodes exploring a diversity of genres (mostly comedy, horror, science fiction and fantasy).');
-        $program->setCategory($this->getReference('category_Animation'));
-        $manager->persist($program);
-
-        $program = new Program();
-        $program->setTitle('Sense8');
-        $program->setSynopsis('Random individuals from accross the globe get suddenly mentally and emotionally connected together.');
-        $program->setCategory($this->getReference('category_Sci-Fi'));
-        $manager->persist($program);
+        foreach (self::PROGRAMS as $programName => $programCategory) {
+            $program = new Program();
+            $program->setTitle($programName);
+            $program->setSynopsis($faker->paragraphs(1, true));
+            $program->setCategory($this->getReference('category_' . $programCategory));
+            $this->addReference('program_' . $programName, $program);
+            $manager->persist($program);
+        }
 
         $manager->flush();
+        
+        // $program = new Program();
+        // $program->setTitle('The Walking Dead');
+        // $program->setSynopsis('Zombie apocalypse, humans have to survive, together as well as against each other.');
+        // $program->setCategory($this->getReference('category_Action'));
+        // $this->addReference('program_0', $program);
+        // $manager->persist($program);
+
+        // $program = new Program();
+        // $program->setTitle('Breaking Bad');
+        // $program->setSynopsis('High school teacher turns to drug manufacturing following his cancer diagnosis.');
+        // $program->setCategory($this->getReference('category_Drama'));
+        // $this->addReference('program_1', $program);
+        // $manager->persist($program);
+
+        // $program = new Program();
+        // $program->setTitle('Better Call Saul');
+        // $program->setSynopsis('Spin-off focusing on the lawyer character of Breaking Bad.');
+        // $program->setCategory($this->getReference('category_Drama'));
+        // $this->addReference('program_2', $program);
+        // $manager->persist($program);
+
+        // $program = new Program();
+        // $program->setTitle('Love, Death and Robots');
+        // $program->setSynopsis('Stand-alone episodes exploring a diversity of genres (mostly comedy, horror, science fiction and fantasy).');
+        // $program->setCategory($this->getReference('category_Animation'));
+        // $this->addReference('program_3', $program);
+        // $manager->persist($program);
+
+        // $program = new Program();
+        // $program->setTitle('Sense8');
+        // $program->setSynopsis('Random individuals from accross the globe get suddenly mentally and emotionally connected together.');
+        // $program->setCategory($this->getReference('category_Sci-Fi'));
+        // $this->addReference('program_4', $program);
+        // $manager->persist($program);
+
+        // $manager->flush();
     }
 
     public function getDependencies()
---
+++ src/DataFixtures/SeasonFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Season;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+use Faker\Factory;
+
+class SeasonFixtures extends Fixture implements DependentFixtureInterface
+{
+    const PROGRAMS = [
+        'The Walking Dead',
+        'Breaking Bad',
+        'Better Call Saul',
+        'Love, Death and Robots',
+        'Sense8',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        foreach (self::PROGRAMS as $key => $programName) {
+            for($i = 1; $i <= 5; $i++) {
+                $season = new Season();
+                $season->setNumber($i);
+                $season->setYear($faker->year());
+                $season->setDescription($faker->paragraphs(3, true));
+    
+                $season->setProgram($this->getReference('program_' . $programName));
+                $this->addReference($programName .'season_' . $i, $season);
+                $manager->persist($season);
+            }
+        }
+        
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProgramFixtures::class,
+        ];
+    }
+}
